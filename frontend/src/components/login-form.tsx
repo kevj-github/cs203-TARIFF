dbb5e155ee27003f706701be9f19bc366ac96540
Krezycat123 <clemirajenkins99@gmail.com>
2025-09-29 01:15:29 +0800

    Fixing some frontend errors

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -2,7 +2,8 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useState } import { Link } from "react-router-dom";
+import { useState } from "react"; 
+import { Link } from "react-router-dom";
 
 interface LoginResponse {
   token: string;
@@ -104,5 +105,3 @@ export function LoginForm({
     </form>
   );
 }
-
-
